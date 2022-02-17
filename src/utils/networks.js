7dc65c1dd263c67e813811e2fc6c231456a446b1
Abdullah Raza <42661870+AlmostEfficient@users.noreply.github.com>
2022-02-17 22:41:39 +1300

    Fix CSS, add networks

---
+++ src/utils/networks.js
@@ -0,0 +1,14 @@
+const networks = {
+  "0x1": "Mainnet",
+  "0x3": "Ropsten",
+  "0x2a": "Kovan",
+  "0x4": "Rinkeby",
+  "0x5": "Goerli",
+  "0x61": "BSC Testnet",
+  "0x38": "BSC Mainnet",
+  "0x89": "Polygon Mainnet",
+  "0x13881": "Polygon Mumbai Testnet",
+  "0xa86a": "AVAX Mainnet",
+}
+
+export { networks };
