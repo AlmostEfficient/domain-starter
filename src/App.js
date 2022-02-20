67d9a880c6cbdcb27219bbf9bf7ad2c57319e59f
Jheave <jimenezjheave123@gmail.com>
2022-02-20 13:25:52 +0800

    update mint domain

--- src/App.js
+++ src/App.js
@@ -113,44 +113,51 @@ const App = () => {
 	const mintDomain = async () => {
 		// Don't run if the domain is empty
 		if (!domain) { return }
+		// Alert the user if the domain is too short
 		if (domain.length < 3) {
 			alert('Domain must be at least 3 characters long');
 			return;
 		}
-
-		// Calculate price based on length of domain
+		// Calculate price based on length of domain (change this to match your contract)	
 		// 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
 		const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
 		console.log("Minting domain", domain, "with price", price);
-	  try {
-
+		try {
 			const { ethereum } = window;
-			if (ethereum) {
-			  const provider = new ethers.providers.Web3Provider(ethereum);
-			  const signer = provider.getSigner();
-			  const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI.abi, signer);
-			
+			(ethereum) {
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+	
 				console.log("Going to pop wallet now to pay gas...")
-			  let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
+				  let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
+				  // Wait for the transaction to be mined
 				const receipt = await tx.wait();
+	
+				// Check if the transaction was successfully completed
 				if (receipt.status === 1) {
 					console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
-
+					
 					// Set the record for the domain
 					tx = contract.setRecord(domain, record);
 					await tx.wait();
+	
 					console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
-
+					
+					// Call fetchMints after 2 seconds
+					setTimeout(() => {
+						fetchMints();
+					}, 2000);
+	
 					setRecord('');
 					setDomain('');
-				}
-				else {
+				} else {
 					alert("Transaction failed! Please try again");
 				}
 			}
-	  } catch(error){
+		  } catch(error) {
 			console.log(error);
-	  }
+		  }
 	}
 
 	const updateDomain = async () => {
