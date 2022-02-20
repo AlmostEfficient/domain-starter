9c7adb7268828c92110d9f73473411924b08e517
Jheave <jimenezjheave123@gmail.com>
2022-02-20 13:19:37 +0800

    update dpmain function

--- src/App.js
+++ src/App.js
@@ -151,6 +151,31 @@ const App = () => {
 			console.log(error);
 	  }
 	}
+	
+	const updateDomain = async () => {
+		if (!record || !domain) { return }
+		setLoading(true);
+		console.log("Updating domain", domain, "with record", record);
+		  try {
+			const { ethereum } = window;
+			if (ethereum) {
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+	
+				let tx = await contract.setRecord(domain, record);
+				await tx.wait();
+				console.log("Record set https://mumbai.polygonscan.com/tx/"+tx.hash);
+	
+				fetchMints();
+				setRecord('');
+				setDomain('');
+			}
+		  } catch(error) {
+			console.log(error);
+		  }
+		setLoading(false);
+	}
 
 	// Render methods
 	const renderNotConnectedContainer = () => (
